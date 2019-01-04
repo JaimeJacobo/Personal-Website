827721abe2ae6f730ef1b60d326a1e0a8db970a6
Jaime Jacobo <jaimejacobordv@gmail.com>
2019-01-04 03:57:13 -0500

    working on the main structure of the page

--- script.js
+++ script.js
@@ -1,2 +1,32 @@
 
-console.log('cuasimodo')
+
+
+document.onload = setTimeout(() =>{
+  $('.introductionText').addClass('fadeOut');
+},3000)
+
+
+// NAVBAR PRUEBAS
+
+$(window).scroll(function() {
+  // 100 = The point you would like to fade the nav in.
+    
+    if ($(window).scrollTop() > 100 ){
+      
+       $('.bg').addClass('showNav');
+      
+    } else {
+      
+      $('.bg').removeClass('showNav');
+      
+     };   	
+  });
+  
+  $('.scroll').on('click', function(e){		
+      e.preventDefault()
+      
+    $('html, body').animate({
+        scrollTop : $(this.hash).offset().top
+      }, 3000);
+  });
+  
