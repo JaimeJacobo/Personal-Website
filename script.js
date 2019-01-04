18d6783c87bf41b1a16d9180884a980c8673b848
Jaime Jacobo <jaimejacobordv@gmail.com>
2019-01-04 15:51:23 -0500

    adding effects to the navbar

--- script.js
+++ script.js
@@ -11,16 +11,16 @@ document.onload = setTimeout(() =>{
 $(window).scroll(function() {
   // 100 = The point you would like to fade the nav in.
     
-    if ($(window).scrollTop() > 100 ){
+  if ($(window).scrollTop() > 100 ){
       
-       $('.bg').addClass('showNav');
+      $('.bg').addClass('showNav');
       
-    } else {
+  } else {
       
-      $('.bg').removeClass('showNav');
+    $('.bg').removeClass('showNav');
       
-     };   	
-  });
+    };   	
+});
   
   $('.scroll').on('click', function(e){		
       e.preventDefault()
@@ -29,4 +29,73 @@ $(window).scroll(function() {
         scrollTop : $(this.hash).offset().top
       }, 3000);
   });
+
+  // FINAL NAVBAR PRUEBAS
+
+
+  // EFECTO HOVER PRUEBAS
+
+
+  
+//This makes the rest of the Navbar dissapear when you hover over 'Contact'
+$('#navbarContact').hover(function(){
+  $('#navbarSocialMedia').addClass('fadeOut');
+  $('#navbarMyProjects').addClass('fadeOut');
+  $('#navbarAboutMe').addClass('fadeOut');
+  }, function(){
+  $('#navbarSocialMedia').removeClass('fadeOut');
+  $('#navbarSocialMedia').addClass('fadeIn');
+  $('#navbarMyProjects').removeClass('fadeOut');
+  $('#navbarMyProjects').addClass('fadeIn');
+  $('#navbarAboutMe').removeClass('fadeOut');
+  $('#navbarAboutMe').addClass('fadeIn');
+});
+
+
+//This makes the rest of the Navbar dissapear when you hover over 'Social Media'
+$('#navbarSocialMedia').hover(function(){
+  $('#navbarContact').addClass('fadeOut');
+  $('#navbarMyProjects').addClass('fadeOut');
+  $('#navbarAboutMe').addClass('fadeOut');
+  }, function(){
+  $('#navbarContact').removeClass('fadeOut');
+  $('#navbarContact').addClass('fadeIn');
+  $('#navbarMyProjects').removeClass('fadeOut');
+  $('#navbarMyProjects').addClass('fadeIn');
+  $('#navbarAboutMe').removeClass('fadeOut');
+  $('#navbarAboutMe').addClass('fadeIn');
+});
+
+
+//This makes the rest of the Navbar dissapear when you hover over 'My Projects'
+$('#navbarMyProjects').hover(function(){
+  $('#navbarContact').addClass('fadeOut');
+  $('#navbarSocialMedia').addClass('fadeOut');
+  $('#navbarAboutMe').addClass('fadeOut');
+  }, function(){
+  $('#navbarContact').removeClass('fadeOut');
+  $('#navbarContact').addClass('fadeIn');
+  $('#navbarSocialMedia').removeClass('fadeOut');
+  $('#navbarSocialMedia').addClass('fadeIn');
+  $('#navbarAboutMe').removeClass('fadeOut');
+  $('#navbarAboutMe').addClass('fadeIn');
+});
+
+
+//This makes the rest of the Navbar dissapear when you hover over 'About Me'
+$('#navbarAboutMe').hover(function(){
+  $('#navbarContact').addClass('fadeOut');
+  $('#navbarSocialMedia').addClass('fadeOut');
+  $('#navbarMyProjects').addClass('fadeOut');
+  }, function(){
+  $('#navbarContact').removeClass('fadeOut');
+  $('#navbarContact').addClass('fadeIn');
+  $('#navbarSocialMedia').removeClass('fadeOut');
+  $('#navbarSocialMedia').addClass('fadeIn');
+  $('#navbarMyProjects').removeClass('fadeOut');
+  $('#navbarMyProjects').addClass('fadeIn');
+});
+
+
+
   
